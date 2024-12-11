8f26c85576ccb98026d07a609e31ef6312ea7de4
Hien <phanquanghien02@gmai.com>
2024-12-11 17:13:21 +0700

    welcome to HealthCare

---
+++ app/index.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Image, Text, View } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+const slides = [
+    {
+        key: 'one',
+        title: 'WELCOME TO HEALTH MANAGEMENT APP',
+        text: 'A place where you can monitor your health, plan for your activity, choose a healthy diet.',
+        image: require('../assets/images/onImage1.png'),
+    },
+    {
+        key: 'two',
+        title: 'PLAN YOUR HEALTH',
+        text: 'Plan your health and fitness activities and track your progress.',
+        image: require('../assets/images/onImage2.png'),
+    },
+    {
+        key: 'three',
+        title: 'ENJOY A HEALTHIER LIFE TODAY',
+        text: 'Let’s build a balanced and healthier lifestyle together.',
+        image: require('../assets/images/onImage3.png'),
+    }
+]
+
+export default class App extends Component {
+
+    renderItem = ({ item }) => {
+        return (
+            <View style={{ flex: 1 }}>
+                <Image
+                    source={item.image}
+                    style={{
+                        resizeMode: 'cover',
+                        height: '60%',
+                        width: '100%'
+                    }}
+                />
+                <Text
+                    style={{
+                        paddingTop: 95,
+                        paddingBottom: 10,
+                        fontSize: 23,
+                        fontWeight: 'bold',
+                        color: '#21465B',
+                        alignSelf: 'center'
+                    }}>
+                    {item.title}
+                </Text>
+                <View>
+                    <Text
+                        style={{
+                            textAlign: 'center',
+                            color: '#b5b5b5',
+                            fontSize: 15,
+                            paddingHorizontal: 30
+                        }}>
+                        {item.text}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+
+    render() {
+        return (
+            <AppIntroSlider
+                renderItem={this.renderItem}
+                data={slides}
+                activeDotStyle={{
+                    backgroundColor: '#21465B',
+                    width: 30
+                }}
+            />
+        )
+    }
+}
